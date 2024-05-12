e371673c400890302e8690f9fd8b575e7c675e94
박륜 <parkryun@baglyun-ui-MacBookAir.local>
2024-05-12 14:02:09 +0900

    first commit

---
+++ express.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require("cors")
+const app = express()
+const port = 3000
+const path = require('path')
+
+const testApi = require('./test.js')
+const matchFreelancerApi = require('./router/matchFreelancer.js')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/test', testApi)
+app.use('/matchFreelancer', matchFreelancerApi)
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+  })
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
